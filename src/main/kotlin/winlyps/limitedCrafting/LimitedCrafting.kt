c701622a9d1e009265aca5f989f4b37126c8c64b
Winlypse <nts3@o2.pl>
2024-10-22 14:57:48 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/limitedCrafting/LimitedCrafting.kt
@@ -0,0 +1,14 @@
+package winlyps.limitedCrafting
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LimitedCrafting : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
